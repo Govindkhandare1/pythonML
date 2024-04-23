2cf62a4b45bfd5339204b9a4d9b16b4a6a06b64c
Govind Khandare <govindkhandare007@gmail.com>
2024-04-23 22:08:16 +0530

    New File added

---
+++ python for ML.py
@@ -0,0 +1,94 @@
+
+
+#What is python
+
+#python is simple and easy
+
+#* free and open source
+
+#* High level Language
+
+#Developed by Guido van Rossum
+
+#python is portable
+
+#First Program
+
+print("Hello world Govind")
+
+# python character set
+
+#letters - A to Z , a to z
+
+#Digit - 0 to 9 
+#Special Symbol - + - * / etc.
+# whitespaces -- blank space , tab , carriage return ,newline,formfeed
+
+#other characters - python can process all ASCII and Unicode characteres as part of data or literals
+
+
+print("Govind is MY name","my age is 27")
+
+print(10+20)
+
+
+# variable
+#A variable is a name given to a memory location in a program
+name= "Bhola Govind"  #string
+Age= 27
+price= 12.33
+
+print(name,Age)
+
+print("My name is : ",name)
+print("my age is : " ,Age)
+
+print(type(name))
+print(type(Age))
+print(type(price))
+
+
+# data Types
+
+#integers
+#positive , negative , 0 (25,-25,0)
+
+#String "Govind" "Hello"   ""  ''  ''' '''
+name = "GK"
+name2= 'GK'
+name3= '''GK'''
+
+#Float
+
+#1.22 any decimal value
+
+#Boolean = True and False (First letter should be capital)
+
+#none   no value stored
+
+
+#keywords
+
+#reserved words --and ,as assert,break class, continue,def,del,elif,else,except,finally,False,for,from,global,if,import,in,is,lambda
+#nonlocal,None,not,or,pass,raise,return,True,try,with,while,yield
+
+a=2
+b=5
+sum =a+b
+print(sum)
+
+#type of Tokens
+
+#Punctuators 
+
+#Punctuators are symbol to organize sentence structure in programming
+
+#(),{},@,[],#, etc. and -=,+=,/=,*=,//=,= etc
+
+###################################################################
+
+# Time 50 min completed and Expression Execution chaper learn tom
+
+
+
+
