21cd1d0d7eb2af9a53d61665761770514d8050ae
Govind Khandare <govindkhandare007@gmail.com>
2024-05-01 01:58:27 +0530

    1 may

--- govind.py
+++ govind.py
@@ -1,12 +1,1 @@
-'''clever if/ Ternary operator
-
-<var> = (false_val,true_val) <condition>
-
-
-age = int(input("age:"))
-vote = ("yes","no")[age<=18]
-print(vote) '''
-
-sal = float(input("salary :"))
-tax = sal*(0.1 ,0.2) [sal>=50000]
-print(tax)
+print(input("name :"))
