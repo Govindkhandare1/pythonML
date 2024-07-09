e3c5c01d2bc0365ba3f4c94d97d75c25773d5391
Govind Khandare <govindkhandare007@gmail.com>
2024-07-09 22:32:49 +0530

    sample commit

--- govind.py
+++ govind.py
@@ -1,7 +1,23 @@
+
+
+
 num = (1,4,9,16,25,36,49,64,81,100,36)
 x = 36
 i = 0  # initialization
 while i < len(num):
     if(num[i]==x):
-        print("index found", i)
+        print("index found", i) 
+    i += 1
+
+
+
+
+
+
+num = (1,4,9,16,25,36,49,64,81,100,36)
+x = 36
+i = 0  # initialization
+while i < len(num):
+    if(num[i]==x):
+        print("index found", i) 
     i += 1
